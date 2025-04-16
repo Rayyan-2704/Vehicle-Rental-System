8cda685129506a5a423b85ccaabb66598e2b90ab
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-04-16 17:28:13 +0500

    Created Customer header file (changes required)

---
+++ Customer.h
@@ -0,0 +1,32 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <conio.h>
+#include <cstdlib>
+
+#include "User.h"
+using namespace std;
+
+class Customer : public User 
+{
+    static int customersCount;
+    // vector for bookings
+
+public:
+    Customer();
+    Customer(const string &name, const string &email, const string &pass, const string &phoneNum, const string &address);
+
+    void generateID() override;
+    void rentVehicle(); // vector of bookings and vehicles
+    void returnVehicle(); // vector of bookings
+    void viewAllBookings(); // vector of bookings
+    void userConsole() override; // vector of bookings & vehicles in parameter
+    void editDetails() override;
+};
+
+#endif
