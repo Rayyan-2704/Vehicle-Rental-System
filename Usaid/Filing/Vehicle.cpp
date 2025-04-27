241b7b20fde36c2b9e97727ad80f5f35b50bdc02
Muhammad Usaid Khan <muhammedusaid.khan@gmail.com>
2025-04-27 12:53:47 +0500

    Create Vehicle.cpp

---
+++ Usaid/Filing/Vehicle.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iomanip>
+#include <cctype>
+#include <conio.h>
+#include <cstdlib>
+
+#include "Vehicle.h"
+using namespace std;
+
+Vehicle::Vehicle() : brand(""), model(""), licensePlate(""), ratePerDay(0.0), isAvailable(false)
+{
+    vehiclesCount++;
+    generateVehicleID();
+}
+
+Vehicle::Vehicle(const string &b, const string &m, const string &l, double rate, bool available, const string vT) : brand(b), model(m), licensePlate(l), ratePerDay(rate), isAvailable(available), vehicleType(vT)
+{
+    vehiclesCount++;
+    generateVehicleID();
+}
+
+void Vehicle::generateVehicleID() 
+{
+    stringstream ss;
+    ss << "V-" << setw(4) << setfill('0') << vehiclesCount;
+    vehicleID = ss.str();
+}
+
+/* Setters */
+void Vehicle::setBrand(const string &b) { brand = b; }
+void Vehicle::setModel(const string &m) { model = m; }
+void Vehicle::setLicensePlate(const string &l) { licensePlate = l; }
+void Vehicle::setRatePerDay(double rate) { ratePerDay = rate; }
+void Vehicle::setAvailability(bool available) { isAvailable = available; }
+void Vehicle::setVehicleType(const string &type) { vehicleType = type; } 
+
+/* Getters */
+string Vehicle::getVehicleID() const { return vehicleID; }
+string Vehicle::getBrand() const { return brand; }
+string Vehicle::getModel() const { return model; }
+string Vehicle::getLicensePlate() const { return licensePlate; }
+double Vehicle::getRatePerDay() const { return ratePerDay; }
+bool Vehicle::getAvailability() const { return isAvailable; }
+string Vehicle::getVehicleType() const { return vehicleType; }
+int Vehicle::getVehiclesCount() { return vehiclesCount; }
+
+Vehicle::~Vehicle() { vehiclesCount--; }
+
+int Vehicle::vehiclesCount = 0;
+
+ostream &operator<<(ostream &os, const Vehicle &v)
+{
+    v.displayVehicleDetails(os);
+    return os;
+}
+void Vehicle::serialize(std::ostream& os) const {
+    os << id << ' ' 
+       << make << ' ' 
+       << model << ' ' 
+       << year << ' ' 
+       << pricePerDay << ' ' 
+       << isAvailable << '\n';
+}
+
+void Vehicle::deserialize(std::istream& is) {
+    is >> id >> make >> model >> year >> pricePerDay >> isAvailable;
+}
+
+void Vehicle::saveToFile(const std::vector<Vehicle>& vehicles, const std::string& filename) {
+    std::ofstream outFile(filename, std::ios::binary);
+    if (!outFile) {
+        throw std::runtime_error("Cannot open file for writing: " + filename);
+    }
+    
+    for (const auto& vehicle : vehicles) {
+        vehicle.serialize(outFile);
+    }
+}
+
+std::vector<Vehicle> Vehicle::loadFromFile(const std::string& filename) {
+    std::vector<Vehicle> vehicles;
+    std::ifstream inFile(filename, std::ios::binary);
+    
+    if (!inFile) {
+        throw std::runtime_error("Cannot open file for reading: " + filename);
+    }
+    
+    Vehicle temp;
+    while (inFile.peek() != EOF) {
+        temp.deserialize(inFile);
+        vehicles.push_back(temp);
+    }
+    
+    return vehicles;
+}
